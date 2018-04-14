bde3e23d0d58ba86562a883f9f6dade456496143
Erik Kristensen <erik@erikkristensen.com>
2018-04-14 15:17:35 -0600

    fixing bug with saltstack.list file

--- sift-cli.js
+++ sift-cli.js
@@ -173,6 +173,10 @@ function fileExists (path) {
 function saltCheckVersion (path, value) {
   return new Promise((resolve, reject) => {
     fs.readFile(path, 'utf8', (err, contents) => {
+      if (err && err.code === 'ENOENT') {
+        return resolve(false);
+      }
+
       if (err) {
         return reject(err);
       }
